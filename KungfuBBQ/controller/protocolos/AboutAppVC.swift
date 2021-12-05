d1346c64ecd8af4da2bcc57c21c8e373680ed789
dgmieth <dgmieth@gmail.com>
2021-12-05 11:00:18 -0300

    adding SYSTEM variable to update app vesrion automatically on view controller

--- KungfuBBQ/controller/protocolos/AboutAppVC.swift
+++ KungfuBBQ/controller/protocolos/AboutAppVC.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class AboutAppVC: UIViewController {
+    
+    @IBOutlet var versionLbl: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let nsObject: AnyObject? = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as AnyObject
+        if let version = nsObject {
+            versionLbl.text = "version \(version)"
+        }else{
+            versionLbl.text = "version 1.0"
+        }
         
     }
     @IBAction func linkClick(_ sender: Any) {
